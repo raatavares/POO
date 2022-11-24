78f9100e3a4622d293ad994ab0342b9c4835729f
Dany99486 <117843005+Dany99486@users.noreply.github.com>
2022-11-24 22:01:56 +0000

    Terminado

--- Reserva.cpp
+++ Reserva.cpp
@@ -30,9 +30,14 @@ void Reserva::recebeComando(const string &frase) {
         arg.push_back(n);
     }
     comand=*arg.begin();
-    
+
+
     if(comand=="animal" && i==4) cout<<"valido"<<endl;
-    if(comand=="animal" && i==2) cout<<"valido"<<endl;
+    else if(comand=="animal" && i==2) cout<<"valido"<<endl;
+
+    else if(comand=="animal_teste" && i==10) criaAnimal(*(arg.begin()+1)->c_str(),stoi(*(arg.begin()+2)),stoi(*(arg.begin()+3)),*(arg.begin()+4),stoi(*(arg.begin()+5)),stoi(*(arg.begin()+6)),stoi(*(arg.begin()+7)),stoi(*(arg.begin()+8)),stoi(*(arg.begin()+9)));
+    else if(comand=="food_teste" && i==8) criaAlimento(*(arg.begin()+1)->c_str(),stoi(*(arg.begin()+2)),stoi(*(arg.begin()+3)),stoi(*(arg.begin()+4)),stoi(*(arg.begin()+5)),stoi(*(arg.begin()+5)),*(arg.begin()+6));
+
 
     else if(comand=="kill"&&i==3) cout<<"valido"<<endl;
     else if(comand=="killid"&&i==2) cout<<"valido"<<endl;
@@ -50,6 +55,8 @@ void Reserva::recebeComando(const string &frase) {
 
     else if(comand=="see"&&i==3) cout<<"valido"<<endl;
 
+    else if(comand=="ver_reserva"&&i==1) verReserva();
+
     else if(comand=="info"&&i==2) cout<<"valido"<<endl;
 
     else if(comand=="n"&&i==1) cout<<"valido"<<endl;
@@ -64,7 +71,7 @@ void Reserva::recebeComando(const string &frase) {
 
     else if(comand=="restore"&&i==2) cout<<"valido"<<endl;
 
-    else if(comand=="load"&&i==2) cout<<"valido"<<endl;                // <- Fazer método para receber comandos por ficheiro
+    else if(comand=="load"&&i==2) recebeComandoPorFicheiro(*(arg.begin()+1));              // <- Fazer método para receber comandos por ficheiro
 
     else if(comand=="slide"&&i==3) cout<<"valido"<<endl;
 
@@ -91,6 +98,42 @@ void Reserva::defineConstante(const string &frase) {
         }
     }
 
+}
+
+void Reserva::recebeComandoPorFicheiro(const string &ficheiro) {
+    ifstream file;
+    string linha;
+    file.open(ficheiro);
+    if (!file.is_open())
+        cout<<"Erro ao abrir ficheiro de comandos"<<endl;
+    else{
+        while (getline(file,linha)){
+            recebeComando(linha);
+        }
+        file.close();
+    }
+}
+
+void Reserva::criaAnimal(const char &especie,int x,int y,const string &nome,int saude,int peso,int fome,const int &id,const int &mov_dist) {
+    Animal a(especie,Coord(x,y),nome,saude,peso,fome,id,mov_dist);
+    animais.push_back(a);
+}
+
+void Reserva::criaAlimento(const char &tipo, int x, int y, int val_nutritivo, int toxicidade, const int &tempo,
+                           const string &cheiro) {
+    Alimento a(tipo,Coord(x,y),val_nutritivo,toxicidade,tempo,cheiro);
+    alimentos.push_back(a);
+}
+
+void Reserva::verReserva() const {
+    cout<<"----------- Animais -----------"<<endl<<endl;
+    for(auto &it:animais){
+        cout<<it.getAsString()<<endl;
+    }
+    cout<<endl<<"---------- Alimentos ----------"<<endl<<endl;
+    for(auto &it:alimentos){
+        cout<<it.getAsString()<<endl;
+    }
 }
 
 
--- Reserva.h
+++ Reserva.h
@@ -11,10 +11,17 @@ class Reserva {
     vector<Alimento>alimentos;
     int SCoelho=20,SOvelha=30,SLobo=25,SCanguru=20;
     const int VCoelho=30,VOvelha=35,VLobo=40,VCanguru=70;
+
+    void criaAnimal(const char &especie,int x,int y,const string &nome,int saude,int peso,int fome,const int &id,const int &mov_dist);
+    void criaAlimento(const char &tipo,int x,int y,int val_nutritivo,int toxicidade,const int &tempo,const string &cheiro);
+
+    void recebeComandoPorFicheiro(const string &frase);
+    void defineConstante(const string &frase);
+    void verReserva() const;
 public:
     Reserva();
     void recebeComando(const string &frase);
-    void defineConstante(const string &frase);
+
 };
 
 
